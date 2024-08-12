0713e727df920f801c256179ea68d1d2c5420a73
Michelle Nguyen <rnb90@txstate.edu>
2024-08-12 12:45:41 -0500

    8. Remove white space in string

---
+++ src/RemoveSpaceString.java
@@ -0,0 +1,24 @@
+// How do you remove spaces from a string in Java?
+// The following example code shows one way to remove spaces from a string using with the Character.isWhitespace() method:
+
+public class RemoveSpaceString {
+    public static void main(String[] args){
+        String str = "A white fox";
+
+        System.out.println(removeWhiteSpace(str));
+    }
+
+    public static String removeWhiteSpace(String input){
+        StringBuilder output = new StringBuilder();
+
+        char[] charArray = input.toCharArray();
+
+        for (char c: charArray){
+            if (!Character.isWhitespace(c)){
+                output.append(c);
+            }
+        }
+
+        return output.toString();
+    }
+}
