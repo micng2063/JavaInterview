ae4cb427807856657c6b61b44fabbcc08960468f
Michelle Nguyen <rnb90@txstate.edu>
2024-08-12 14:03:52 -0500

    18. Calculate sum array

---
+++ src/SumArray.java
@@ -0,0 +1,17 @@
+// How do you get the sum of all elements in an integer array in Java?
+
+// You can use a for loop to iterate over the array elements and add them to get the final sum:
+
+public class SumArray {
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5};
+
+        int sum = 0;
+
+        for (int i : array){
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
