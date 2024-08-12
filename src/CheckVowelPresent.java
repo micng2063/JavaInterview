794bfd4b6fdfaaab482ab5f14c214133cd1bab76
Michelle Nguyen <rnb90@txstate.edu>
2024-08-12 11:10:49 -0500

    3. Check if vowel presents

---
+++ src/CheckVowelPresent.java
@@ -0,0 +1,12 @@
+// How do you swap two numbers without using a third variable in Java?
+
+public class CheckVowelPresent {
+    public static void main(String[] args){
+        System.out.println(containVowel("Hello"));
+        System.out.println(containVowel("TV"));
+    }
+
+    public static boolean containVowel(String input){
+        return input.toLowerCase().matches(".*[aeiou].*");
+    }
+}
