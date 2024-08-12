473cd4a60fa186d3f2b933fccd5247013fa45066
Michelle Nguyen <rnb90@txstate.edu>
2024-08-12 18:47:09 -0500

    22. Print date in specific format

---
+++ src/FormatDate.java
@@ -0,0 +1,15 @@
+// How do you print a date in specific format in Java?
+// The following example code shows how to use the SimpleDateFormat class to format the date string:
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FormatDate {
+    public static void main(String[] args){
+        String pattern = "MM-dd-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+        String date = simpleDateFormat.format(new Date());
+        System.out.println(date);
+    }
+}
